ea220e011f8a83358c7ecb417cd0d1759e4b284a
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2014-04-26 12:13:07 +0000

    added code in order to demonize nancy

--- nancy/photoRemote/photoRemote/photoRemote/Program.cs
+++ nancy/photoRemote/photoRemote/photoRemote/Program.cs
@@ -8,15 +8,24 @@
 		static void Main(string[] args)
 		{
 			var uri =
-				new Uri("http://localhost:3579");
+				new Uri("http://localhost:8888");
 
 			using (var host = new NancyHost(uri))
 			{
 				host.Start();
 
-				Console.WriteLine("Your application is running on " + uri);
-				Console.WriteLine("Press any [Enter] to close the host.");
-				Console.ReadLine();
+				if (args.Any(s => s.Equals("-d", StringComparison.CurrentCultureIgnoreCase)))
+	            {
+	                Thread.Sleep(Timeout.Infinite);
+	            }
+	            else
+	            {
+					Console.WriteLine("Your application is running on " + uri);
+					Console.WriteLine("Press any [Enter] to close the host.");
+					Console.ReadLine();
+					host.Stop();
+	            }
+
 			}
 		}
 	}
