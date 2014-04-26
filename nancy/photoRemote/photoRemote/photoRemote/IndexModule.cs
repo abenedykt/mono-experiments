2bfbf1051a005ea859526112577a57394746b377
abenedykt <arkadiusz@benedykt.net>
2014-04-26 08:25:36 +0200

    nancy project

---
+++ nancy/photoRemote/photoRemote/photoRemote/IndexModule.cs
@@ -0,0 +1,19 @@
+﻿namespace photoRemote
+{
+	using Nancy;
+
+	public class IndexModule : NancyModule
+	{
+		public IndexModule()
+		{
+			Get["/"] = parameters =>
+			{
+				return View["index"];
+			};
+
+			Get ["/settings"] = _ => {
+				return View ["parameters"];
+			};
+		}
+	}
+}
