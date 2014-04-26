a415285265e2db3fd8afcf1be9847d0e057343c3
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2014-04-26 19:48:45 +0000

    shooting photos with nancy

--- nancy/photoRemote/photoRemote/Bootstrapper.cs
+++ nancy/photoRemote/photoRemote/Bootstrapper.cs
@@ -6,6 +6,6 @@
 	{
 		// The bootstrapper enables you to reconfigure the composition of the framework,
 		// by overriding the various methods and properties.
-		// For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+		// For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper	
 	}
 }
--- nancy/photoRemote/photoRemote/IndexModule.cs
+++ nancy/photoRemote/photoRemote/IndexModule.cs
@@ -1,6 +1,7 @@
 ﻿namespace photoRemote
 {
 	using Nancy;
+	using System.IO;
 
 	public class IndexModule : NancyModule
 	{
@@ -14,6 +15,33 @@
 			Get ["/settings"] = _ => {
 				return View ["parameters"];
 			};
+
+			Get["/photo"] = _ => {
+				MakePhoto();
+				return View[new PhotoModel()];
+			};
+		}
+
+		public void MakePhoto(){
+			var command = "/home/pi/makephoto";
+
+			var proc = new System.Diagnostics.Process();
+			proc.StartInfo.FileName = "bash";
+			proc.StartInfo.Arguments = "'" + command +  "'";
+			proc.StartInfo.UseShellExecute =false;
+			proc.StartInfo.CreateNoWindow = true;
+
+			proc.Start();
+
+		}
+
+
+	}
+
+	public class PhotoModel{
+		public string ImagePath {
+			get{ return "image.jpg";}
+			set{}
 		}
 	}
 }
