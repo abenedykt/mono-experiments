77f851978f0377a76624d6ad4f5f38e475c35a60
abenedykt <arkadiusz@benedykt.net>
2014-05-04 22:58:51 +0200

    added angular js to all photos tab

---
+++ nancy/output/Content/Scripts/app.js
@@ -0,0 +1,29 @@
+﻿function AllPicsCtrl($scope) {
+	$scope.images = {
+		count: 4,
+		items: [
+			{
+				name: 'image1',
+				date: '12-12-2014 12:32',
+				url: 'http://www.onet.pl'
+			},
+			{
+				name: 'image2',
+				date: '12-12-2014 12:33',
+				url: 'http://www.wp.pl'
+			},
+			{
+				name: 'image3',
+				date: '12-12-2014 12:34',
+				url: 'http://www.gazeta.pl'
+			},
+			{
+				name: 'image4',
+				date: '12-12-2014 12:35',
+				url: 'http://www.interia.pl'
+			}
+		]
+
+	};
+}	
+
